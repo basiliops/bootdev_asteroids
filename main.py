e0bf184b0a7ce948eb4f0cc0ec30aabccbca2f73
basiliops <dennis.basilio@pm.me>
2025-05-22 22:37:30 -0700

    Rendered player triangle

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -14,10 +16,11 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # Limit to 60 FPS
-        dt = clock.tick(60) / 1000        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
